75653dac392ba96990b0d96bce8a246417e957c3
jieunjeon <jieunjeon1023@gmail.com>
2021-07-28 23:50:55 +0900

    update time complexity

---
+++ Programmers/42577_phone_book.py
@@ -0,0 +1,40 @@
+
+"""
+https://programmers.co.kr/learn/courses/30/lessons/42577
+
+전화번호 목록
+
+문제 설명
+전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+구조대 : 119
+박준영 : 97 674 223
+지영석 : 11 9552 4421
+전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+
+제한 사항
+phone_book의 길이는 1 이상 1,000,000 이하입니다.
+각 전화번호의 길이는 1 이상 20 이하입니다.
+같은 전화번호가 중복해서 들어있지 않습니다.
+
+입출력 예제
+
+phone_book	return
+["119", "97674223", "1195524421"]	false
+["123","456","789"]	true
+["12","123","1235","567","88"]	false
+"""
+
+def solution(phone_book):
+    """
+    Time Complexity: O(nlogn + n*20) = O(nlogn) where n is the number of elements in the phone_book list, and m is the max length of a phone number.
+    """
+    phone_book_ordered = sorted(phone_book)
+    phone_book_ordered_2 = phone_book_ordered[1:]
+    
+    for i, j in zip(phone_book_ordered, phone_book_ordered_2):
+        if len(j) > len(i) and j.startswith(i):
+            return False
+        
+    return True
